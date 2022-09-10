6c258ec90342b46ec96eb22a9fff535d7a8e1daa
Macy Kay Mannix <macymannix@gmail.com>
2022-09-10 15:35:27 -0700

    links up

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,18 +1,28 @@
 import React from "react";
+import chris from "../../assets/images/chris.jpg";
 
 function Project() {
-  return(
-      <section>
-          <div className="center">
-        Fan Page 
-        <a href="https://macykcool.github.io/fanpage/">
-            <img src="./assets/images/chris.jpg" alt="preview of chris isaak fanpage" />
-            </a>
-            
-          </div>
-      </section>
-  )
+  return (
+    <section>
+      <div>
+        <p>Fan Page</p>
+        <a href="https://macykcool.github.io/fanpage/"> Deployed
+          </a>
+          <a href="https://github.com/macykcool/pro-port"> Repo
+          </a>
+        <p>
+          <a href="https://macykcool.github.io/fanpage/">
+            <img
+              src={chris}
+              className="my-2"
+              style={{ height: "25%", width: "25%" }}
+              alt="demo"
+            />
+          </a>
+        </p>
+      </div>
+    </section>
+  );
 }
-
 
 export default Project;
